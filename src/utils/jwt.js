0b632fd95b35aedf35a9818179f8cb9d8eff0e3e
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-30 12:00:51 +0530

    Updated Commit

---
+++ src/utils/jwt.js
@@ -0,0 +1,13 @@
+const jwt =require('jsonwebtoken');
+
+const generateAccessToken=(payload)=>
+  jwt.sign(payload,process.env.JWT_ACCESS_SECRET,{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRE
+  });
+
+
+const generateRefreshToken=(payload)=>
+  jwt.sign(payload,process.env.JWT_REFRESH_SECRET,{
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRE
+  });
+  module.exports={ generateAccessToken,generateRefreshToken};
